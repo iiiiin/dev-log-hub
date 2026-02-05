239a2b5f01885d2d4971f468e8e11b49b42cc721
iiiiin <iiiiin@users.noreply.github.com>
2026-02-05 09:47:08 +0000

    deploy: ffb2f1242d1806010394dd0a59375e691c3cb66d

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1770282034',resources: [
+    cacheName: 'chirpy-1770284826',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
